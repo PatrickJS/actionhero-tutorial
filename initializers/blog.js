a93367532aacfbac93c1ef348db2d24694d71da2
gdi2290 <github@gdi2290.com>
2014-03-12 21:23:06 -0700

    init blog methods

--- initializers/blog.js
+++ initializers/blog.js
@@ -1,6 +1,41 @@
 exports.blog = function(api, next){
 
-  api.blog = {};
+  api.blog = {
+    // Constants
+    separator: ';',
+    postPrefix: 'posts',
+    commentPrefix: 'comments',
+    // Posts
+    postAdd: function(userName, title, content, next) {
+
+    },
+
+    postView: function(userName, title, next) {
+
+    },
+
+    postsList: function(userName, next) {
+
+    },
+
+    postEdit: function(userName, title, content, next) {
+
+    },
+
+    postDelete: function(userName, title, next) {
+
+    },
+    // Comments
+    commentAdd: function(userName, title, commentorName, comment, next) {
+
+    },
+    commentsView: function(userName, title, next) {
+
+    },
+    commentDelete: function(userName, title, commentId, next) {
+
+    }
+  };
 
   api.blog._start = function(api, next){
     next();
